687ae1b00ee9beb7e36171cbc5d107fc76975789
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-08-22 00:00:20 +0300

    add the quotes array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+ quote:Quote[]=[
+{name:'Sea swim',author:'Ben Carson',quote:'If you swim in the sea,be ready for the waves',submitter:'Varshita Donavan'},
+{name:'Space',author:'Neil armstrong',quote:'If you want to go to space,you have to be ready for uncomfortable situations',submitter:'Sajad Abdi'},
+{name:'Get more',author:'Kahiji Kiuot',quote:'You can only get more if you have less on your side',submitter:'Carl Odinga'}
+ ];
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    name:string;
+    author:string;
+    quote:string;
+    submitter:string;
 }
