55914884f5bd057db47f04722f5743a5b304cd35
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-08-24 00:20:41 +0300

    add a new quote

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -13,7 +13,8 @@ export class BookComponent implements OnInit {
     // {name:'Get more',author:'Kahiji Kiuot',quote:'You can only get more if you have less on your side',submitter:'Carl Odinga'}
     new Quote('Sea Swim','Ben Carson','If you swim in the sea,be ready for the waves','Varshita Donavan',new Date (2002,7,20)),
     new Quote('Space','Neil Armstrong','If you want to go to space,you have to be ready for uncomfortable situations','Sajad Abdi',new Date (1990,8,8)),
-    new Quote('Get more','Kahiji Kiuot','You can only get more if you have less on your side','Carl Odinga',new Date (20069,11)),
+    new Quote('Get more','Kahiji Kiuot','You can only get more if you have less on your side','Carl Odinga',new Date (2006,9,11)),
+    new Quote('Love yourself','Rihanna','The minute you learn to love yourself you wont want to be anyone else','Kariuki Onyango',new Date(2016,2,8)),
      ];
 
      toggleDetails(index){
